92835bf97a9d2638ab469bf2494b49aec29594be
Dedy Setiawan <glempangchild@gmail.com>
2020-11-26 15:11:42 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime aut eveniet itaque officia voluptatibus earum omnis fugiat quidem tempora blanditiis provident dolorum, labore possimus nam pariatur quam odit debitis molestiae?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
