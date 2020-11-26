09a418bdc7a802b461c13c07645e15eff7a74c06
Dedy Setiawan <glempangchild@gmail.com>
2020-11-26 15:02:28 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime aut eveniet itaque officia voluptatibus earum omnis fugiat quidem tempora blanditiis provident dolorum, labore possimus nam pariatur quam odit debitis molestiae?</p>
 </body>
 </html>
